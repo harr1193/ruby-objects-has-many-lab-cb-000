894e49ad93242a7bd728f28c083da147dc0f02f7
Kyle Harris <k_harris95@live.com>
2018-03-28 05:49:58 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count
 
   def initialize(name)
     @name = name
@@ -13,10 +14,15 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
+  end
+
+  def post_count
+
   end
 end
